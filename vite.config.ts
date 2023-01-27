a7e2a9928303dde1f9264e0471f81c5bea17e0f4
Corbin Crutchley <git@crutchcorn.dev>
2023-01-27 06:53:21 -0800

    Add DTS generation, externalize Zod

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 import {resolve} from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -7,7 +8,9 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({
+    entryRoot: resolve(__dirname, "./lib"),
+  })],
   resolve: {
     alias: {
       'uniform': resolve(__dirname, "./lib")
@@ -20,10 +23,11 @@ export default defineConfig({
       fileName: 'uniform',
     },
     rollupOptions: {
-      external: ['react'],
+      external: ['react', 'zod'],
       output: {
         globals: {
           react: 'React',
+          zod: 'zod',
         },
       },
     },
