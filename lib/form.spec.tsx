2f04c1335cb67bf2c9c9ec07fddf3e002d6fd1b5
Corbin Crutchley <git@crutchcorn.dev>
2023-01-27 08:49:35 -0800

    Add tests for onChange and onSubmit validation tests

--- lib/form.spec.tsx
+++ lib/form.spec.tsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import {Form} from "./form";
 import {Field, SubmitField} from "./fields";
 import {render, waitFor} from "@testing-library/react";
+import { z } from "zod";
 
 test("Form should render children", () => {
     const {getByText} = render(<Form onSubmit={(_) => {
@@ -44,6 +45,52 @@ test("Form should submit with values in tact", async () => {
     `));
 });
 
-test.todo("Form should not submit if there are errors with onChangeValidate");
+test("Form should not submit if there are errors with onChangeValidate", async () => {
+    const SubmitValues = () => {
+        const [values, setValues] = useState<string | null>(null);
 
-test.todo("Form should not submit if there are errors with onSubmitValidate");
+        if (values) return <p>{values}</p>
+
+        return (
+            <Form onSubmit={(values) => setValues(JSON.stringify(values))}>
+                <Field<string> name={"email"} initialValue="" onChangeValidate={z.string().min(1)}>
+                    {() => <></>}
+                </Field>
+                <SubmitField>
+                    {({onSubmit}) => <button onClick={onSubmit}>Submit</button>}
+                </SubmitField>
+            </Form>
+        )
+    }
+
+    const {getByText} = render(<SubmitValues/>);
+
+    await user.click(getByText("Submit"));
+
+    expect(getByText("Submit")).toBeInTheDocument();
+});
+
+test("Form should not submit if there are errors with onSubmitValidate", async () => {
+    const SubmitValues = () => {
+        const [values, setValues] = useState<string | null>(null);
+
+        if (values) return <p>{values}</p>
+
+        return (
+            <Form onSubmit={(values) => setValues(JSON.stringify(values))}>
+                <Field<string> name={"email"} initialValue="" onSubmitValidate={z.string().min(1)}>
+                    {() => <></>}
+                </Field>
+                <SubmitField>
+                    {({onSubmit}) => <button onClick={onSubmit}>Submit</button>}
+                </SubmitField>
+            </Form>
+        )
+    }
+
+    const {getByText} = render(<SubmitValues/>);
+
+    await user.click(getByText("Submit"));
+
+    expect(getByText("Submit")).toBeInTheDocument();
+});
