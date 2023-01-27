ecefff14321ad45db1123c25a1d6a7650d2d0edb
Corbin Crutchley <git@crutchcorn.dev>
2023-01-26 17:51:20 -0800

    Add second test

--- src/lib/index.spec.tsx
+++ src/lib/index.spec.tsx
@@ -1,17 +1,34 @@
 // @vitest-environment jsdom
-import {expect, test} from "vitest";
-import {render} from "@testing-library/react";
-import {Form} from "./index";
+import {afterEach, expect, test} from "vitest";
+import {render, cleanup} from "@testing-library/react";
+import {Field, Form} from "./index";
 
 import matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
-test("Form should render children", () => {
-        const {getByText} = render(<Form onSubmit={(_) => {
-        }}>
-            <div>Test</div>
-        </Form>)
+afterEach(() => {
+    cleanup();
+})
 
-        expect(getByText("Test")).toBeInTheDocument();
+test("Form should render children", () => {
+    const {getByText} = render(<Form onSubmit={(_) => {
+    }}>
+        <div>Test</div>
+    </Form>)
+
+    expect(getByText("Test")).toBeInTheDocument();
+})
+
+test("Field should render children", () => {
+    const {getByText} = render(<Form onSubmit={(_) => {
+    }}>
+        <Field name={"test"}>
+            {() => (
+                <div>Test</div>
+            )}
+        </Field>
+    </Form>)
+
+    expect(getByText("Test")).toBeInTheDocument();
 })
