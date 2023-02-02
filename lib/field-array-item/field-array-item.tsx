f442856d2d8fe109e30822c76c83618c741965f7
Corbin Crutchley <corbin.crutchley@nwyc.com>
2023-02-01 23:35:30 -0500

    Add better setValue logic for array field item

--- lib/field-array-item/field-array-item.tsx
+++ lib/field-array-item/field-array-item.tsx
@@ -1,8 +1,20 @@
-import React, { useContext, useMemo, useState } from "react";
-import { ZodError, ZodType } from "zod";
+import React, {
+  ForwardedRef,
+  forwardRef,
+  memo,
+  useCallback,
+  useContext,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from "react";
+import { ZodError } from "zod";
 import {
   FieldArrayInstance,
+  FieldInstance,
+  FieldInstanceProps,
   fillPath,
+  FormContext,
   getPath,
   getValidationError,
   stringToPath,
@@ -10,35 +22,40 @@ import {
 } from "houseform";
 import { FieldArrayContext } from "../field-array/context";
 
-export function FieldArrayItem<T>({
-  children,
-  name,
-  onChangeValidate,
-}: {
-  children: ({
-    setValue,
-  }: {
-    setValue: (v: T) => void;
-    errors: string[];
-    value: T;
-  }) => React.ReactNode;
-  name: string;
-  onChangeValidate?: ZodType;
-}) {
+export interface FieldArrayItemRenderProps<T = any>
+  extends FieldInstanceProps<T> {
+  children: (props: FieldInstance<T>) => JSX.Element;
+}
+
+export function FieldArrayItemComp<T>(
+  props: FieldArrayItemRenderProps<T>,
+  ref: ForwardedRef<FieldInstance<T>>
+) {
+  const { name } = props;
+
+  const _normalizedDotName = useMemo(() => {
+    return stringToPath(name).join(".");
+  }, [name]);
+
   const array = useContext(FieldArrayContext) as FieldArrayInstance<T>;
+  const formContext = useContext(FormContext);
 
   const [errors, setErrors] = useState<string[]>([]);
+  const [isTouched, setIsTouched] = useState<boolean>(false);
+  const [isDirty, setIsDirty] = useState<boolean>(false);
 
   const fullAccessorPath = useMemo(() => {
     const arrayNamePathArr = stringToPath(array.props.name);
-    const fieldItemPathArr = stringToPath(name);
+    const fieldItemPathArr = stringToPath(props.name);
     for (const i of arrayNamePathArr) {
       if (i !== fieldItemPathArr.shift()) {
-        throw new Error("Invalid name");
+        throw new Error(
+          "You must prepend the FieldArrayItem name with the name of the parent FieldArray"
+        );
       }
     }
     return fieldItemPathArr;
-  }, [array.props.name, name]);
+  }, [array.props.name, props.name]);
 
   const itemIndex = useMemo(() => {
     return parseInt(fullAccessorPath[0]);
@@ -52,20 +69,82 @@ export function FieldArrayItem<T>({
     return getPath(array.value[itemIndex] as object, accessorPath.join("."));
   }, [array.value, itemIndex]);
 
-  function setValue(v: T) {
-    const vv = { ...array.value[itemIndex] } as object;
-    fillPath(vv, accessorPath.join("."), v);
-    array.setValue(itemIndex, vv as T);
-    if (onChangeValidate) {
-      validate(v, null as any, onChangeValidate)
-        .then(() => {
-          setErrors([]);
-        })
-        .catch((error: string | ZodError) => {
-          setErrors(getValidationError(error as ZodError | string));
-        });
-    }
-  }
+  const setValue = useCallback(
+    (v: T | ((prevState: T) => T)) => {
+      const isPrevAFunction = (t: any): t is (prevState: T) => T =>
+        typeof t === "function";
 
-  return <>{children({ setValue, errors, value })}</>;
+      const newVal = isPrevAFunction(v) ? v(array.value[itemIndex]) : v;
+
+      const newArrayObject = { ...array.value[itemIndex] } as object;
+      fillPath(newArrayObject, accessorPath.join("."), newVal);
+      array.setValue(itemIndex, newArrayObject as T);
+
+      setIsDirty(true);
+      setIsTouched(true);
+
+      /**
+       * Call `listenTo` field subscribers for other fields.
+       *
+       * Placed into a `setTimeout` so that the `setValue` call can finish before the `onChangeListenerRefs` are called.
+       */
+      setTimeout(() => {
+        formContext.onChangeListenerRefs.current[props.name]?.forEach((fn) =>
+          fn()
+        );
+      }, 0);
+
+      if (props.onChangeValidate) {
+        validate(newVal, null as any, props.onChangeValidate)
+          .then(() => {
+            setErrors([]);
+          })
+          .catch((error: string | ZodError) => {
+            setErrors(getValidationError(error as ZodError | string));
+          });
+      }
+    },
+    [props.name, props.onChangeValidate]
+  );
+
+  const isValid = useMemo(() => {
+    return errors.length === 0;
+  }, [errors]);
+
+  const fieldArrayInstance = useMemo(() => {
+    return {
+      setValue,
+      errors,
+      value,
+      _normalizedDotName,
+      onBlur: () => {},
+      props,
+      isTouched,
+      isValid,
+      isDirty,
+      setIsDirty,
+      setErrors,
+      setIsTouched,
+    };
+  }, [
+    setValue,
+    errors,
+    value,
+    _normalizedDotName,
+    props,
+    isTouched,
+    isValid,
+    isDirty,
+    setIsDirty,
+    setErrors,
+    setIsTouched,
+  ]);
+
+  useImperativeHandle(ref, () => fieldArrayInstance, [fieldArrayInstance]);
+
+  return props.children(fieldArrayInstance);
 }
+
+export const FieldArrayItem = memo(forwardRef(FieldArrayItemComp)) as <T>(
+  props: FieldArrayItemRenderProps<T> & { ref?: ForwardedRef<FieldInstance<T>> }
+) => ReturnType<typeof FieldArrayItemComp>;
