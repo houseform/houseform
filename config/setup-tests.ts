516f0d7f637354c10a8fed1a8ed271231ec8e5cc
Corbin Crutchley <git@crutchcorn.dev>
2023-02-18 03:31:50 -0800

    chore: migrate to non-fork Jest DOM matcher

--- config/setup-tests.ts
+++ config/setup-tests.ts
@@ -1,7 +1,11 @@
-import { afterEach, expect } from "vitest";
-import matchers from "@testing-library/jest-dom/matchers";
-import userEvent from "@testing-library/user-event";
+// vitest-setup.ts
+import { expect, afterEach } from "vitest";
+import matchers, {
+  TestingLibraryMatchers,
+} from "@testing-library/jest-dom/matchers";
+
 import { cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 expect.extend(matchers);
 
@@ -10,3 +14,12 @@ globalThis.user = userEvent.setup();
 afterEach(() => {
   cleanup();
 });
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Vi {
+    interface JestAssertion<T = any>
+      extends jest.Matchers<void, T>,
+        TestingLibraryMatchers<T, void> {}
+  }
+}
--- config/tests.d.ts
+++ config/tests.d.ts
@@ -1,6 +1,7 @@
 import { UserEvent } from "@testing-library/user-event";
 
 declare global {
+  // eslint-disable-next-line no-var
   var user: UserEvent;
 }
 
