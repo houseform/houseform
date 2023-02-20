c0a4fb35cf1115c45618fbebfe975cc0d040ccb1
Corbin Crutchley <corbin.crutchley@nwyc.com>
2023-02-20 00:32:24 -0800

    docs: finalize homepage design

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,9 +2,20 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import { EnhanceAppContext } from "vitepress";
 import ClickToIFrame from "./ClickToIFrame.vue";
+import { h } from 'vue'
+import HomePage from './HomePage.vue';
+
+const modules = import.meta.glob('../../../.all-contributorsrc', { as: 'raw' })
+
+const allContributors = await modules["../../../.all-contributorsrc"]();
 
 export default {
   ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'home-features-after': () => h(HomePage, {allContributors: JSON.parse(allContributors)}),
+    })
+  },
   enhanceApp(ctx: EnhanceAppContext) {
     DefaultTheme.enhanceApp(ctx)
     ctx.app.component('ClickToIFrame', ClickToIFrame)
