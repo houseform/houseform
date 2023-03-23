d08bffd3f6a2f959d5d3b7314514779376953186
Corbin Crutchley <git@crutchcorn.dev>
2023-03-22 18:02:50 -0700

    chore: initial tests for ESLint rule, not including exhaustivity

--- rules/exhaustive-child-deps.spec.ts
+++ rules/exhaustive-child-deps.spec.ts
@@ -3,36 +3,88 @@ import { describe, test } from "vitest";
 import { ESLintUtils } from "@typescript-eslint/utils";
 import { rule } from "./exhaustive-child-deps";
 
-describe("ESLint", () => {
-  test("Tests rules", () => {
-    const ruleTester = new ESLintUtils.RuleTester({
-      parser: "@typescript-eslint/parser",
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    });
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaFeatures: { jsx: true } },
+});
 
+describe("ESLint - Exhaustive Child Deps", () => {
+  test("Should not error on valid JSX", () => {
     ruleTester.run("exhaustive-child-deps", rule, {
       valid: [
         `
-        <Form memoChild={["Test"]}>
-          {() => "Hello"}
+      <div></div>
+      `,
+      ],
+      invalid: [],
+    });
+  });
+
+  test("Form with no function body should not error", () => {
+    ruleTester.run("exhaustive-child-deps", rule, {
+      valid: [
+        `
+        <Form>
+          <div></div>
         </Form>
       `,
-        "<div><div></div></div>",
       ],
-      invalid: [
-        {
-          code: `
-        <Form memoChild={["Test"]}>
-          {() => "Test"}
+      invalid: [],
+    });
+  });
+
+  test("Form with no memoChild should not error", () => {
+    // We're relying on the TypeScript compiler to catch this
+    ruleTester.run("exhaustive-child-deps", rule, {
+      valid: [
+        `
+      <Form>
+        {() => <div></div>}
+      </Form>
+      `,
+      ],
+      invalid: [],
+    });
+  });
+
+  test("Form with no function body but memoChild should not error", () => {
+    // We're relying on the TypeScript compiler to catch this
+    ruleTester.run("exhaustive-child-deps", rule, {
+      valid: [
+        `
+        <Form memoChild={[]}>
+          <div></div>
         </Form>
-          `,
-          errors: [
-            {
-              messageId: "exhaustiveChildDeps",
-            },
-          ],
-        },
+      `,
       ],
+      invalid: [],
+    });
+  });
+
+  test("Form with non-array memoChild should not error", () => {
+    // We're relying on the TypeScript compiler to catch this
+    ruleTester.run("exhaustive-child-deps", rule, {
+      valid: [
+        `
+      <Form memoChild={"Test"}>
+        {() => <div></div>}
+      </Form>
+      `,
+      ],
+      invalid: [],
+    });
+  });
+
+  test("Form with empty memoChild and no through vars should not error", () => {
+    ruleTester.run("exhaustive-child-deps", rule, {
+      valid: [
+        `
+      <Form memoChild={[]}>
+        {() => <div></div>}
+      </Form>
+      `,
+      ],
+      invalid: [],
     });
   });
 });
